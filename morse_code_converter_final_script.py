079a1c6d4f1d3cb2bd8164eab8f1e2fe63c8a24d
meyavuz <meyavuz@users.noreply.github.com>
2022-09-03 17:06:36 -0700

    Add mac support
    Ben de mac oldugu icin calistiramadim. Biraz degistirdim code'u mac eklemek icin ama yapilacak baska olaylar da var. Onlari da ekleyecegim. 
    
    * Mesela `morse_transformer(message)` ile local bir `wav` file dump edebiliyor musun? Eger olursa onu ekleyip pygame ile caldirabilirim.

--- morse_code_converter_final_script.py
+++ morse_code_converter_final_script.py
@@ -1,16 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug 13 15:53:09 2022
-
-@author: oguztuzun
-"""
-
+import sys
 from morse_transformer import morse_transformer
 from morse_plotter import plotter
-from sound_player import sound_player
 
-message = input('Enter your message: ')
 
-plotter(morse_transformer(message))  
+if sys.platform == "win32":
+    print("Win32")
+    from sound_player import sound_player
+elif sys.platform == "darwin":
+    print("Mac")
+    import pygame
+else:
+    print("Not defined")
 
-sound_player(morse_transformer(message))
+
+# message = input('Enter your message: ')
+message = "sos"
+
+plotter(morse_transformer(message))
+
+if sys.platform == "win32":
+    print("Win32")
+    sound_player(morse_transformer(message))
+
+elif sys.platform == "darwin":
+    print("Mac")
+
+    pygame.init()
+
+    gameDisplay = pygame.display.set_mode((640, 480))
+
+    pygame.mixer.init()
+    # pygame.mixer.music.load("morse_sound_long.wav")
+    pygame.mixer.music.load(morse_transformer(message))
+    pygame.mixer.music.play()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
